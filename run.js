c0c1466f21777ee8a8742f2582320be6bb11a1c4
Chiro <Chiro2001@163.com>
2022-06-18 16:58:30 +0800

    count stock count

--- run.js
+++ run.js
@@ -4,21 +4,30 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoURI = "mongodb://localhost:27017/dipiper";
 
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(() => resolve(), ms));
+}
+
+
 async function updateStockList(db) {
     const stockList = await dip.stock.symbols.getStockList();
     const col = db.collection("stockList");
     stockList.forEach(stockItem => {
         col.updateOne({ symbol: stockItem.symbol }, { $set: stockItem }, { upsert: true });
-        logger.info("got", stockItem, "done");
+        logger.info("save stock item ", stockItem, " done");
     });
     console.log("updateStockList done.");
+}
+
+async function getStockList(db) {
+
 }
 
 async function getDB() {
     return await new Promise((resolve, reject) => {
         MongoClient.connect(mongoURI, function (err, db) {
             if (err) throw err;
-            console.log("数据库已创建!");
+            console.log("Database init done!");
             resolve(db);
         });
     });
@@ -27,7 +36,13 @@ async function getDB() {
 async function main() {
     const database = await getDB();
     const db = database.db("dipiper");
-    await updateStockList(db);
+    logger.info("hi");
+    let stockCount = await db.collection("stockList").countDocuments();
+    if (stockCount < 3000) await updateStockList(db);
+    stockCount = await db.collection("stockList").countDocuments();
+    logger.info("stock count: ", stockCount);
+    await sleep(1000);
+    process.exit(0);
 }
 
 main();
